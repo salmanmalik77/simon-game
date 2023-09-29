88e039f0e6e8cf06fcb720b4eb1dae03162835a8
salmanmalik77 <144127772+salmanmalik77@users.noreply.github.com>
2023-09-29 23:12:17 +0530

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).click(function () {
+$(document).tap(function () {
   $("level-title").text("level "+ level);
 
   nextSequence();
